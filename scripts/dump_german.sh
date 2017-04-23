e6aafd40247b5db91bbf8bd4e7ac0ee2a8a80fff
Stijn Voss <stijnvoss@Stijns-MBP.fritz.box>
2017-04-23 21:59:34 +0200

    Dataset notebooks, german

--- scripts/dump_german.sh
+++ scripts/dump_german.sh
@@ -1,2 +1,2 @@
-/home/stijn/Documents/chainer/bin/python /home/stijn/Documents/mount/unsupervised-language-translation-using-rnn/code/export_dump.py https://dumps.wikimedia.org/dewiki/20170401/dewiki-20170401-pages-articles3.xml-p1877053p3377053.bz2 result-german/data.npz --seq 1000000 --min_th 5
+python code/export_dump.py https://dumps.wikimedia.org/dewiki/20170401/dewiki-20170401-pages-articles3.xml-p1877053p3377053.bz2 result-german/data.npz --seq 1000000 --min_th 5
 
